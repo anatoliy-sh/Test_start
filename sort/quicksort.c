f39c4dff5748186df93fdb89ada667969a0ef99c
anatoliy-sh <an_sheb@mail.ru>
2015-10-02 14:27:31 +0300

    Исправлена реализация shared Memory. Добавлены 2 процесса (один создает, другой сортирует).

--- sort/quicksort.c
+++ sort/quicksort.c
@@ -3,7 +3,9 @@
 #include <sys/shm.h>
 #include <stdio.h>
 #include <errno.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 void exit (int code);
 
@@ -38,71 +40,64 @@ void qs(int* s_arr, int first, int last)
 
 int main()
 {
-      int *array; /* Указатель на разделяемую память */
-      int shmid; /* IPC дескриптор для области разделяемой памяти */
-      int new = 1; /* Флаг необходимости инициализации элементов массива */
-      char pathname[] = "prog.c"; /* Имя файла, использующееся для генерации ключа.
-       Файл с таким именем должен существовать в текущей директории */
+  int fd[2];
+  pipe(fd);
+  int memsize = 15;
+  int sizeOfShm = sizeof(int)*memsize;
+  int shmid = shmget(IPC_PRIVATE, sizeOfShm, SHM_W | SHM_R | IPC_CREAT);
+  if(shmid<0){
+    perror("Impossible to access shared memory");
+    exit(EXIT_FAILURE);
+  }
+  int* sharedMem = shmat(shmid, NULL, 0); //SHM_RDONLY
 
-      key_t key; /* IPC ключ */
-      if((key = ftok(pathname,0)) < 0)
-      {
-           printf("Can't generate keyn");
-           exit(-1);
-      }
+  if(!fork()){
+       
+    close(fd[0]);
+    int first = 0;
+    int n = 15;
+    //int a[] = {14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+    //sharedMem = a;
 
-      /* Cоздаем разделяемую память для сгенерированного ключа, т.е. если для этого ключа она уже существует
-      системный вызов вернет отрицательное значение. Размер памяти определяем как размер массива из 3-х целых переменных,
-      права доступа 0666 - чтение и запись разрешены для всех */
+    for(int i = 0; i < memsize; ++i)
+      sharedMem[memsize-i-1] = i;
 
-      if((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0)
-      {
-            /* В случае возникновения ошибки пытаемся определить: возникла ли она из-за того, что сегмент
-            разделяемой памяти уже существует или по другой причине */
-            if(errno != EEXIST)
-            {
-              /* Если по другой причине - прекращаем работу */
-              printf("Can't create shared memoryn");
-              exit(-1);
-            }
-            else
-            {
-              /* Если из-за того, что разделяемая память уже существует пытаемся получить ее IPC дескриптор и,
-              в случае удачи, сбрасываем флаг необходимости инициализации элементов массива */
+    shmdt(&shmid);      
+    write(fd[1], &first, sizeof(int));
 
-                if((shmid = shmget(key, 3*sizeof(int), 0)) < 0)
-                {
-                    printf("Can't find shared memoryn");
-                    exit(-1);
-                }
-                new = 0;
-            }
-       }
+    write(fd[1], &n, sizeof(int));
 
-        /* Пытаемся отобразить разделяемую память в адресное пространство текущего процесса. Обратите внимание на то,
-         что для правильного сравнения мы явно преобразовываем значение -1 к указателю на целое.*/
 
-        if((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1))
-        {
-            printf("Can't attach shared memoryn");
-            exit(-1);
-        }
+    }else{
+      close(fd[1]);
+      int first;
+      int last;
+      int nbytes = read(fd[0], &first, sizeof(first));
+      printf("Received string: %d\n", first);
+      nbytes = read(fd[0], &last, sizeof(last));
+      printf("Received string: %d\n", last);
+      for(int i = 0; i < memsize; ++i)
+        printf("%d ", sharedMem[i]);
+      printf("\n");
+      shmdt(&shmid);
+      shmctl(shmid,IPC_RMID,NULL); //IPC_STAT IPC_SET
+      qs(sharedMem,first,last-1);
 
-        /*  Работа с участком памяти */
-        int n = 10;
-        int a[] = {9,8,7,6,5,4,3,2,1,0};
-        //for(int i = 0; i<10; i++){
-          printf("%d",a[0]);
-        //}
+      for(int i = 0; i < memsize; ++i)
+        printf("%d ", sharedMem[i]);
+      printf("\n");
+    }
         //array = a;
-        qs(a,0,9);
-        //for(int i = 0; i<10; i++){
-          printf("%d",a[0]);
-        //}
-        if(shmdt(array) < 0)
-        {
-            printf("Can't detach shared memoryn");
-            exit(-1);
-        }
+        /*qs(a,0,9);
+
+
+        for(int i = 0; i<n; i++){
+        printf("%d \n",a[i]);
+        }*/
+
+
+
+
+
         return 0;
 } 
